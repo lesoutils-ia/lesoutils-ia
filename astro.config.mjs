f9e26beadbec92a58283c2c67e19b54ad1c59791
lesoutils-ia <lesoutilsia1@gmail.com>
2026-04-02 15:15:28 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://lesoutils-ia.fr',
+  redirects: {
+    '/outils': '/comparatifs',
+    '/automatisation': '/comparatifs/automatisation',
+    '/redaction-ia': '/comparatifs/redaction-ia',
+  },
 });
